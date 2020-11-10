13a1b87ea4de7d0bd51b9d25349d91f2a3061240
Seongwon Park <seongwon.park@navercorp.com>
2020-11-10 16:12:52 +0900

    add puts

--- Sources/fetch_app_status.rb
+++ Sources/fetch_app_status.rb
@@ -69,8 +69,7 @@ token = Spaceship::ConnectAPI::Token.create(
 
 
 Spaceship::ConnectAPI.token = token 
-print("=====Token=====")
-print(token)
+puts token
 bundle_id_array = bundle_id.to_s.split(",")
 
 if bundle_id_array.length.zero?

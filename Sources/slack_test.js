860abfcfc4e6355463b7b487ccaeb868d5c480fe
Seongwon Park <seongwon.park@navercorp.com>
2020-11-11 08:54:03 +0900

    add error log

--- Sources/slack_test.js
+++ Sources/slack_test.js
@@ -6,13 +6,15 @@ function post() {
 			'X-SENDER': 'slack',
 			'Content-Type': 'application/json'
 		},
-		url: 'http://ikameshi.linecorp.com/privmsg',
+		url: 'http://ikameshi.linecorp.com/notice',
 		form: {
 			channel: 'testnel',
 			message: 'TestTest',	
 			nickname: 'iOS App Test'
 		}
 	}, function(error, response, body){
+		console.log(error);
+		console.log(response);
 		console.log(body);
 	});
 }
